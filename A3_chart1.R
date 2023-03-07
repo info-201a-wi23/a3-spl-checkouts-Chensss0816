378f40c68eb78bc3a352e43911aef40a8bb9ac2a
Yuchen AN <ayc0816@uw.edu>
2023-03-06 22:45:29 -0800

    Revise analysis according to comments

--- A3_chart1.R
+++ A3_chart1.R
@@ -3,18 +3,27 @@ library("dplyr")
 
 spl_data <- read.csv("/Users/anyuchen/desktop/2017-2023-10-Checkouts-SPL-Data 2.csv", stringsAsFactors = FALSE)
 
-sum_each_type <- spl_data %>%
-  group_by(MaterialType) %>% summarise(total_num = sum(Checkouts, na.rm = TRUE))
+type_checkouts <- spl_data %>%
+  filter(MaterialType %in% c("BOOK", "EBOOK", "MAGAZINE", "AUDIOBOOK", "SOUNDDISC", "VIDEODISC")) %>%
+  select("MaterialType", "CheckoutYear", "Checkouts")
 
-type_checkouts <- sum_each_type %>%  filter(MaterialType %in% c("BOOK", "EBOOK", "MAGAZINE", "AUDIOBOOK", "SOUNDDISC", "VIDEODISC"))
+total_type_checkouts <- type_checkouts %>%
+  group_by(CheckoutYear, MaterialType) %>%
+  summarize(total_type_checkouts = sum(Checkouts))
 
-bar_plot <- ggplot(data = type_checkouts) +
-  geom_col(mapping = aes(
-    x = MaterialType,
-    y = total_num,
-    fill = MaterialType
-  )) +
-  labs(title = "Total checkouts from 2017 to 2023",
-       x = "Medium Type",
-       y = "Total Checkouts",
-       fill = "Medium Type")
+
+stacked_bar_plot <- ggplot(data = total_type_checkouts) +
+  geom_col(
+    aes(
+      x = CheckoutYear,
+      y = total_type_checkouts,
+      fill = MaterialType
+    ),
+    position = "fill"
+  ) +
+  labs(
+    title = "Relative checkouts from 2017 to 2023",
+    x = "Year",
+    y = "Total Checkouts",
+    fill = "Medium Type"
+  )
--- summary.R
+++ summary.R
@@ -3,15 +3,16 @@ library("dplyr")
 
 spl_data <- read.csv("/Users/anyuchen/desktop/2017-2023-10-Checkouts-SPL-Data 2.csv", stringsAsFactors = FALSE)
 
-most_checkout_each_year <- spl_data %>% group_by(CheckoutYear) %>% filter(Checkouts == max(Checkouts))
+most_checkout_each_year <- spl_data %>% group_by(CheckoutYear) %>% filter(Checkouts == max(Checkouts)) %>% pull(Checkouts)
 
-distinct_checkout_each_year <- spl_data %>% group_by(CheckoutYear) %>% summarise(distinct_checkout = n_distinct(Title))
+distinct_checkout_each_year <- spl_data %>% group_by(CheckoutYear) %>% summarise(distinct_checkout = n_distinct(Title)) %>% pull(distinct_checkout)
 
-month_most_checkouts <- spl_data %>% group_by(CheckoutYear, CheckoutMonth) %>% summarize(checkout_month = sum(Checkouts)) %>% filter(checkout_month == max(checkout_month))
+month_most_checkouts <- spl_data %>% group_by(CheckoutYear, CheckoutMonth) %>% summarize(checkout_month = sum(Checkouts)) %>% filter(checkout_month == max(checkout_month)) %>% pull(CheckoutMonth)
 
-item_types <- spl_data %>% summarise(item_types = n_distinct(MaterialType))
+item_types <- spl_data %>% summarise(item_types = n_distinct(MaterialType)) %>%  pull(item_types)
 
-most_popular_book <- spl_data %>% group_by(CheckoutYear) %>% filter(MaterialType == "EBOOK") %>% filter(Checkouts == max(Checkouts))
+most_popular_book <- spl_data %>% group_by(CheckoutYear) %>% filter(MaterialType == "EBOOK") %>% filter(Checkouts == max(Checkouts)) %>% pull(Title)
+
 
  
 
